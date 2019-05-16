764252ed71c3d749cddfd01a52af7ee2cc388fb7
Marcin <elomelo302@gmail.com>
2019-05-17 00:32:09 +0200

    Updates

---
+++ precache-manifest.4c17a42031b99d5fce57b7039958549b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce280db7dd89fd432af7a04a8b0206bc",
+    "url": "/Star-Wars/index.html"
+  },
+  {
+    "revision": "21380b6caa5aa188c061",
+    "url": "/Star-Wars/static/css/main.4f425633.chunk.css"
+  },
+  {
+    "revision": "f8709620f0d0a45e081d",
+    "url": "/Star-Wars/static/js/2.1fa8d4cc.chunk.js"
+  },
+  {
+    "revision": "21380b6caa5aa188c061",
+    "url": "/Star-Wars/static/js/main.149d8b63.chunk.js"
+  },
+  {
+    "revision": "dc2da87b4f775f210e81",
+    "url": "/Star-Wars/static/js/runtime~main.47cb02c2.js"
+  },
+  {
+    "revision": "b07b0442bd794e2d093c02d61aa1066e",
+    "url": "/Star-Wars/static/media/blue-lightsaber.b07b0442.png"
+  },
+  {
+    "revision": "9881417c60f4d8a8eb1185d6a0f3db78",
+    "url": "/Star-Wars/static/media/red-lightsaber.9881417c.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/star-wars/precache-manifest.cab173d58069b3941d44a168ac3b4359.js"
+  "/Star-Wars/precache-manifest.4c17a42031b99d5fce57b7039958549b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/star-wars/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Star-Wars/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
